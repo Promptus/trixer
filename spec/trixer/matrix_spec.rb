d409273fcb6eea57cd05184c62173445ef9256bb
Lars Kuhnt <lars.kuhnt@gmail.com>
2021-10-08 12:20:27 +0200

    fixed matrix

--- spec/trixer/matrix_spec.rb
+++ spec/trixer/matrix_spec.rb
@@ -3,7 +3,6 @@ RSpec.describe Trixer::Matrix do
   describe '::from_adjacency_list' do
 
     let(:objects) { %w(1 2 3 4 5 6) }
-    let(:object_set) { objects }
     let(:adjacency_list) do
       {
         '1' => ['2', '4', '5'],
@@ -15,7 +14,7 @@ RSpec.describe Trixer::Matrix do
       }
     end
 
-    subject { Trixer::Matrix.from_adjacency_list(adjacency_list: adjacency_list, objects: object_set) }
+    subject { Trixer::Matrix.from_adjacency_list(adjacency_list: adjacency_list, objects: objects) }
 
     it do
       is_expected.to eql([
@@ -29,8 +28,41 @@ RSpec.describe Trixer::Matrix do
       ])
     end
 
+    context  do
+      let(:objects) { %w(1 2 3) }
+      let(:adjacency_list) do
+        {
+          '1' => %w(3),
+          '2' => [],
+          '3' => []
+        }
+      end
+      it do
+        is_expected.to eql([
+         # 1 2 3
+          [0,0,1], # 1
+          [0,0,0], # 2
+          [1,0,0], # 3
+        ])
+      end
+
+      context 'reversed objects' do
+        let(:objects) { %w(3 2 1) }
+
+        it do
+          is_expected.to eql([
+           # 1 2 3
+            [0,0,1], # 1
+            [0,0,0], # 2
+            [1,0,0], # 3
+          ])
+        end
+      end
+
+    end
+
     context 'subset of objects' do
-      let(:object_set) { ['1', '2', '4', '6'] }
+      let(:objects) { %w(1 2 4 6) }
 
       it do
         is_expected.to eql([
